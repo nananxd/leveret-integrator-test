0aa8b8d11291a92c6bab3b0c2bed904915f7f7df
rolandpilar <mccoyo51313@gmail.com>
2023-12-21 21:48:51 +0800

    minor changes. better button function

--- Assets/Scripts/Test/TestScript.cs
+++ Assets/Scripts/Test/TestScript.cs
@@ -5,9 +5,50 @@ using UnityEngine.UI;
 public class TestScript : MonoBehaviour
 {
     [SerializeField] PlayableDirector timeline;
+    [SerializeField] Button timelineButton; 
+
+    private bool isTimelinePlaying = false;
+
+    private void Start()
+    {
+        timelineButton.interactable = true;
+    }
+
+    private void OnEnable()
+    {
+        timeline.played += OnTimelinePlayed;
+        timeline.stopped += OnTimelineStopped;
+    }
+
+    private void OnDisable()
+    {
+        timeline.played -= OnTimelinePlayed;
+        timeline.stopped -= OnTimelineStopped;
+    }
 
     public void TimelineButtonTapped()
     {
-        timeline.Play();
+        if (!isTimelinePlaying)
+        {
+            timeline.Play();
+            timelineButton.interactable = false;
+        }
+    }
+
+    private void OnTimelinePlayed(PlayableDirector director)
+    {
+        isTimelinePlaying = true;
+    }
+
+    private void OnTimelineStopped(PlayableDirector director)
+    {
+        isTimelinePlaying = false;
+        StartCoroutine(EnableButtonAfterTimeline());
+    }
+
+    private System.Collections.IEnumerator EnableButtonAfterTimeline()
+    {
+        yield return null;
+        timelineButton.interactable = true;
     }
 }
